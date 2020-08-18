8b502228f8662b2cc571deb2fa8cba5b300c8b54
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-08-19 08:09:46 +1000

    added some stlying to the github battle react component

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 
 class App extends React.Component {
   render() {
@@ -9,7 +10,7 @@ class App extends React.Component {
 
     return (
       <div className="container">
-        <Popular />
+        <Battle />
       </div>
     );
   }
