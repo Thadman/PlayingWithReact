04c03fff3ad5f6984f688fa41edbbacd8eef0a11
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-08-29 12:10:21 +1000

    added context , with  a dak theme, had a problem before committing where i had called style instead of className on a div

--- app/index.js
+++ app/index.js
@@ -3,16 +3,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { ThemeProvider } from "./contexts/theme";
+import Nav from "./components/Nav";
 
 class App extends React.Component {
-  render() {
-    const name = "Thad";
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light",
+        }));
+      },
+    };
+  }
+  render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
 ReactDOM.render(<App />, document.getElementById("app"));
+
+// we have wrapped all of our Component in the ThemeProvider passing it the value prop which is set in state within the constructor. Now for any components that we want to have the theme we can wrap them in the ThemeConsumer and they will have access to the theme through the value that was passed into ThemeProvider as a prop. Anytime we invoke the toggleTheme() , any Component that is wrapped inside of the ConsumerTheme will update and therefore change themes.
